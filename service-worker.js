5c39436b9ca49114243f66892d5649e895919f59
Matti Vesa <mattijvesa@gmail.com>
2019-06-07 09:47:47 +0300

    Update demo with V8 export and delete button support

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","4c1ca9b56f7e85705ecedc443c4ad2e6"]];
+var precacheConfig = [["index.html","cfa3bb1661a88b416860e7ebb4fcac2f"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
